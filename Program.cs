b9fd8073c6c798f2cc331241dab563acc0664a23
Jahle <Jahle@ABLACKMAN>
2024-07-09 14:50:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_10._2._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 2, -1, 3, -3, 10, -200 };
+
+            
+            var positiveNumbers = numbers.Where(n => n > 0);
+
+            Console.WriteLine("Positive numbers:");
+            foreach (var num in positiveNumbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
